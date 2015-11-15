254bbd0135e24a4ff483ab5501634ce4b509d85e
David Moothen <dmoothen@websitti.fr>
2015-11-15 16:17:02 +0100

    Display shortcut when scrolling.

--- src/js/jquery.scrolltop.js
+++ src/js/jquery.scrolltop.js
@@ -2,13 +2,16 @@
 
     $.fn.scrolltop = function(params){
 
-        var defaults = {};
+        var defaults = {
+            duration: 1000
+        };
 
         params = $.extend({}, defaults, params);
 
         return this.each(function(){
 
             var $element = $(this);
+            var $window = $(window);
             var $link = $('<a></a>')
             .attr('href', '#')
             .addClass('scrolltop')
@@ -16,10 +19,20 @@
             .click(function(e){
                 e.preventDefault();
                 $('body', 'html').animate({
-                    scrollTop: 0
-                }, 1000);
+                    scrollTop: $element.offset().top
+                }, params.duration);
             })
             .appendTo($element);
+
+            $window.scroll(function(e){
+                var scrollTop = $(this).scrollTop();
+                if (scrollTop > $(this).height() / 2) {
+                    $link.addClass('active');
+                }
+                else {
+                    $link.removeClass('active');
+                }
+            });
 
         });
 
